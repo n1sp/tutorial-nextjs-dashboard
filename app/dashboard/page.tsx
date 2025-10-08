7599ad5010b67ee7dc1a9199e2c4429aaac171ac
n1sp <n1sp.pc001@gmail.com>
2025-10-08 19:45:22 +0900

    Chapter7: Fetching Data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,37 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
+
+export default async function Page() {
+  const revenue = await fetchRevenue(); // 月ごとの収益データ取得
+  const latestInvoices = await fetchLatestInvoices(); // 直近の請求書取得
+  const {
+    totalPaidInvoices,   // 支払済み請求書の合計金額
+    totalPendingInvoices, // 未払い請求書の合計金額
+    numberOfInvoices, // 請求書の総数
+    numberOfCustomers,
+  } = await fetchCardData();
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
